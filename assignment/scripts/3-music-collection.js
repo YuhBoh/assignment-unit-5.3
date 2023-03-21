9d837edcd47fb60c6aa6716f582e213c68fd6b1e
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-03-20 22:40:00 -0700

    added function, addToCollection and filled out code block with requirements

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+album = {Title: title,
+         Artist: artist,
+         Year: yearPublished};
+
+collection.push(album);
+
+return album;
+}
+
+console.log(addToCollection('bob', 'boob', 19));
+console.log(collection);
