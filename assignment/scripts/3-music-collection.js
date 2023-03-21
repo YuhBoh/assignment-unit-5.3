588a8a72307b9a3491e51358f1d4ed4bcce284bd
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-03-21 00:21:17 -0700

    added 6 albums to collection and returned albums and collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,11 @@
 console.log('***** Music Collection *****')
+
+//1.//
+console.log('1.');
 let collection = [];
 
+//2.//
+console.log('2.');
 function addToCollection(title, artist, yearPublished){
 album = {Title: title,
          Artist: artist,
@@ -9,7 +14,22 @@ album = {Title: title,
 collection.push(album);
 
 return album;
+return collection;
 }
 
-console.log(addToCollection('bob', 'boob', 19));
+console.log(addToCollection('Bob', 'Bob Song', 19));
 console.log(collection);
+
+//3.//
+console.log('3.');
+console.log(addToCollection('Boub', 'Boub Song', 1442));
+console.log(addToCollection('Bab', 'Bab Song', 105));
+console.log(addToCollection('Beb', 'Beb Song', 985));
+console.log(addToCollection('Bib', 'Bib Song', 4785));
+console.log(addToCollection('Bubb', 'Bubb Song', 0));
+console.log(collection);
+
+//4.//
+console.log('4.');
+
+
